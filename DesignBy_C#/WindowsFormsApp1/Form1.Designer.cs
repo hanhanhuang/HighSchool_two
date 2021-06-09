3f6ee778cbe1f7b71585099daaa8e4e8e7108eaa
hanhanhuang <75626762+hanhanhuang@users.noreply.github.com>
2021-06-09 20:01:24 +0800

    Add files via upload

---
+++ DesignBy_C#/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.NO1 = new System.Windows.Forms.Button();
+            this.NO9 = new System.Windows.Forms.Button();
+            this.NO6 = new System.Windows.Forms.Button();
+            this.NO3 = new System.Windows.Forms.Button();
+            this.NO8 = new System.Windows.Forms.Button();
+            this.NO5 = new System.Windows.Forms.Button();
+            this.NO2 = new System.Windows.Forms.Button();
+            this.NO7 = new System.Windows.Forms.Button();
+            this.NO4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.NO1);
+            this.groupBox1.Controls.Add(this.NO9);
+            this.groupBox1.Controls.Add(this.NO6);
+            this.groupBox1.Controls.Add(this.NO3);
+            this.groupBox1.Controls.Add(this.NO8);
+            this.groupBox1.Controls.Add(this.NO5);
+            this.groupBox1.Controls.Add(this.NO2);
+            this.groupBox1.Controls.Add(this.NO7);
+            this.groupBox1.Controls.Add(this.NO4);
+            this.groupBox1.Location = new System.Drawing.Point(369, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 349);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // NO1
+            // 
+            this.NO1.Enabled = false;
+            this.NO1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NO1.Location = new System.Drawing.Point(53, 37);
+            this.NO1.Name = "NO1";
+            this.NO1.Size = new System.Drawing.Size(75, 75);
+            this.NO1.TabIndex = 2;
+            this.NO1.UseVisualStyleBackColor = true;
+            this.NO1.Click += new System.EventHandler(this.Button_click);
+            // 
+            // NO9
+            // 
+            this.NO9.Enabled = false;
+            this.NO9.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NO9.Location = new System.Drawing.Point(268, 232);
+            this.NO9.Name = "NO9";
+            this.NO9.Size = new System.Drawing.Size(75, 75);
+            this.NO9.TabIndex = 0;
+            this.NO9.UseVisualStyleBackColor = true;
+            this.NO9.Click += new System.EventHandler(this.Button_click);
+            // 
+            // NO6
+            // 
+            this.NO6.Enabled = false;
+            this.NO6.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NO6.Location = new System.Drawing.Point(268, 132);
+            this.NO6.Name = "NO6";
+            this.NO6.Size = new System.Drawing.Size(75, 75);
+            this.NO6.TabIndex = 0;
+            this.NO6.UseVisualStyleBackColor = true;
+            this.NO6.Click += new System.EventHandler(this.Button_click);
+            // 
+            // NO3
+            // 
+            this.NO3.Enabled = false;
+            this.NO3.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NO3.Location = new System.Drawing.Point(268, 37);
+            this.NO3.Name = "NO3";
+            this.NO3.Size = new System.Drawing.Size(75, 75);
+            this.NO3.TabIndex = 0;
+            this.NO3.UseVisualStyleBackColor = true;
+            this.NO3.Click += new System.EventHandler(this.Button_click);
+            // 
+            // NO8
+            // 
+            this.NO8.Enabled = false;
+            this.NO8.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NO8.Location = new System.Drawing.Point(161, 232);
+            this.NO8.Name = "NO8";
+            this.NO8.Size = new System.Drawing.Size(75, 75);
+            this.NO8.TabIndex = 0;
+            this.NO8.UseVisualStyleBackColor = true;
+            this.NO8.Click += new System.EventHandler(this.Button_click);
+            // 
+            // NO5
+            // 
+            this.NO5.Enabled = false;
+            this.NO5.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NO5.Location = new System.Drawing.Point(161, 132);
+            this.NO5.Name = "NO5";
+            this.NO5.Size = new System.Drawing.Size(75, 75);
+            this.NO5.TabIndex = 0;
+            this.NO5.UseVisualStyleBackColor = true;
+            this.NO5.Click += new System.EventHandler(this.Button_click);
+            // 
+            // NO2
+            // 
+            this.NO2.Enabled = false;
+            this.NO2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NO2.Location = new System.Drawing.Point(161, 37);
+            this.NO2.Name = "NO2";
+            this.NO2.Size = new System.Drawing.Size(75, 75);
+            this.NO2.TabIndex = 0;
+            this.NO2.UseVisualStyleBackColor = true;
+            this.NO2.Click += new System.EventHandler(this.Button_click);
+            // 
+            // NO7
+            // 
+            this.NO7.Enabled = false;
+            this.NO7.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NO7.Location = new System.Drawing.Point(53, 232);
+            this.NO7.Name = "NO7";
+            this.NO7.Size = new System.Drawing.Size(75, 75);
+            this.NO7.TabIndex = 0;
+            this.NO7.UseVisualStyleBackColor = true;
+            this.NO7.Click += new System.EventHandler(this.Button_click);
+            // 
+            // NO4
+            // 
+            this.NO4.Enabled = false;
+            this.NO4.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NO4.Location = new System.Drawing.Point(53, 132);
+            this.NO4.Name = "NO4";
+            this.NO4.Size = new System.Drawing.Size(75, 75);
+            this.NO4.TabIndex = 0;
+            this.NO4.UseVisualStyleBackColor = true;
+            this.NO4.Click += new System.EventHandler(this.Button_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(40, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 47);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sign your name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button12);
+            this.groupBox2.Controls.Add(this.button11);
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(48, 108);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(291, 319);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button12.Location = new System.Drawing.Point(193, 238);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(73, 37);
+            this.button12.TabIndex = 5;
+            this.button12.Text = "exit";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button11.Location = new System.Drawing.Point(103, 238);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 37);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "restart";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button10.Location = new System.Drawing.Point(16, 238);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(66, 37);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "start";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(223, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "WIN:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(223, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "WIN:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(106, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(33, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "O：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(33, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "X ：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(363, 386);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(226, 44);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "TIC TAC TOE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(635, 400);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 26);
+            this.label7.TabIndex = 4;
+            this.label7.Text = " X turn.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "TIC TAC TOE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button NO9;
+        private System.Windows.Forms.Button NO6;
+        private System.Windows.Forms.Button NO3;
+        private System.Windows.Forms.Button NO8;
+        private System.Windows.Forms.Button NO5;
+        private System.Windows.Forms.Button NO2;
+        private System.Windows.Forms.Button NO7;
+        private System.Windows.Forms.Button NO4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button NO1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ DesignBy_C#/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public int player = 2; //玩家
+        public int x = 0;  //x贏的次數
+        public int o = 0; //o贏的次數
+        public int turn = 0; //順序(幾輪
+        
+        
+        
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            this.Close(); //關閉程式
+        }
+
+        private void Button_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender; 
+            if (button.Text=="") //button 已有文字則不能做改動
+            {
+                if (player % 2 == 0)
+                {
+                    turn++; 
+                    if (turn != 9) //turn=9代表九宮格裡皆有文字 不能在輪流
+                    {
+                        label7.Text = " O turn.";
+                    }
+                    else
+                        label7.Text = "";
+                    button.Text = " X ";
+                    player++;
+                }
+                else
+                {
+                    turn++;
+                    if (turn != 9)
+                    {
+                        label7.Text = " X turn.";
+                    }
+                    button.Text = " O ";
+                    player++;
+                    
+                }
+                if (Checkwinner() == true) //區分是誰連線
+                {
+                    if(button.Text==" X ")
+                    {
+                        label7.Text = "";
+                        MessageBox.Show(textBox1.Text + " WIN!!!");
+                        x++;
+                    }
+                    else if(button.Text==" O ")
+                    {
+                        label7.Text = "";
+                        MessageBox.Show(textBox2.Text + " WIN!!!");
+                        o++;
+                    }
+                    label4.Text = "WIN:" + x + "次";
+                    label5.Text = "WIN:" + o + "次";
+                }
+                else if((Checkwinner() == false) && turn==9)
+                {
+                    MessageBox.Show("平手...");
+                }
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        bool Checkwinner() //檢查連線
+        {
+            //行
+            if ((NO1.Text == NO2.Text) && (NO2.Text == NO3.Text) && NO1.Text != "")
+            {
+                NO1.BackColor = NO2.BackColor = NO3.BackColor = Color.Yellow; //標示哪條連線
+                return true;
+            }
+            else if ((NO4.Text == NO5.Text) && (NO5.Text == NO6.Text) && NO4.Text != "")
+            {
+                NO4.BackColor = NO5.BackColor = NO6.BackColor = Color.Yellow;
+                return true;
+            }
+            else if ((NO7.Text == NO8.Text) && (NO8.Text == NO9.Text) && NO7.Text != "")
+            {
+                NO7.BackColor = NO8.BackColor = NO9.BackColor = Color.Yellow;
+                return true;
+            }
+
+            //列
+            if ((NO1.Text == NO4.Text) && (NO4.Text == NO7.Text) && NO1.Text != "")
+            {
+                NO1.BackColor = NO4.BackColor = NO7.BackColor = Color.Yellow;
+                return true;
+            }
+            else if ((NO2.Text == NO5.Text) && (NO5.Text == NO8.Text) && NO2.Text != "")
+            {
+                NO2.BackColor = NO5.BackColor = NO8.BackColor = Color.Yellow;
+                return true;
+            }
+            else if ((NO3.Text == NO6.Text) && (NO6.Text == NO9.Text) && NO3.Text != "")
+            {
+                NO3.BackColor = NO6.BackColor = NO9.BackColor = Color.Yellow;
+                return true;
+            }
+
+            //斜
+            if ((NO1.Text == NO5.Text) && (NO5.Text == NO9.Text) && NO1.Text != "")
+            {
+                NO1.BackColor = NO5.BackColor = NO9.BackColor = Color.Yellow;
+                return true;
+            }
+            else if ((NO3.Text == NO5.Text) && (NO5.Text == NO7.Text) && NO3.Text != "")
+            {
+                NO3.BackColor = NO5.BackColor = NO7.BackColor = Color.Yellow;
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            NO1.Enabled = NO2.Enabled = NO3.Enabled = true;
+            NO4.Enabled = NO5.Enabled = NO6.Enabled = true;
+            NO7.Enabled = NO8.Enabled = NO9.Enabled = true;
+            textBox1.Enabled = textBox2.Enabled = false;
+            button10.Enabled = false;
+        }
+        void Newgame() //重新開始 全部值皆要重設
+        {
+            player = 2;
+            turn = 0;
+            label7.Text = " X turn.";
+            NO1.Text = NO2.Text = NO3.Text = "";
+            NO4.Text = NO5.Text = NO6.Text = "";
+            NO7.Text = NO8.Text = NO9.Text = "";
+
+            NO1.BackColor = NO2.BackColor = NO3.BackColor = Color.Gainsboro;
+            NO4.BackColor = NO5.BackColor = NO6.BackColor = Color.Gainsboro;
+            NO7.BackColor = NO8.BackColor = NO9.BackColor = Color.Gainsboro;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Newgame(); //重新開始 (接續玩家名字以及獲勝次數
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
